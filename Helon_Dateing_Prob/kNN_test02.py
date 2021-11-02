3106a9252a9a6e14325065b1f5d63a951a2a8a7c
Forest_Loong <913679284@qq.com>
2021-11-01 22:25:07 -0600

    2

--- Helon_Dateing_Prob/kNN_test02.py
+++ Helon_Dateing_Prob/kNN_test02.py
@@ -40,10 +40,10 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         # Fetch the category of the first k elements
         voteIlabel = labels[sortedDistIndices[i]]
-        # dict.get(key,default=None),字典的get()方法,返回指定键的值,如果值不在字典中返回默认值。
-        # Count category times数
+        # dict.get(key,default=None),get() method in dictionary,it will return the specified key value. If the value is not in the dictionary, retutn default value.
+        # Count category times
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    # python3中用items()替换python2中的iteritems()
+    # In Python3, replace iteritems() in python2 with items() 
     # key=operator.itemgetter(1) Sorting by dictionary values
     # key=operator.itemgetter(0) Sorting by dictionary keys
     # reverse Descending Sorting Dictionary
@@ -89,7 +89,6 @@ def file2matrix(filename):
         line = line.strip()
         # s.split(str="",num=string,cout(str)) --> Slice the string according to the '\t' separator。
         listFromLine = line.split('\t')
-        # 将数据前三列提取出来,存放到returnMat的NumPy矩阵中,也就是特征矩阵
         # The first three columns of the data are extracted and stored in the NumPy matrix of returnMat,
         # which is the feature matrix
         returnMat[index, :] = listFromLine[0:3]
@@ -106,7 +105,7 @@ def file2matrix(filename):
 
 
 """
-可视化数据
+Data Visualization
 Function Description: Visualization Data
 Parameters:
 	datingDataMat - feature Matrix 
@@ -144,9 +143,9 @@ def showdatas(datingDataMat, datingLabels):
     plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
     plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
 
-    # 画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+    # Plot scatter diagram, use the data in first and the third colum of datingDataMat matrix to plot the figure. The size of the scatter points is 15, transparency is 0.5  
     axs[0][1].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
-    # 设置标题,x轴label,y轴label
+    # Set the titlef for both x-axis and y-axis 
     axs1_title_text = axs[0][1].set_title(u'Frequent flyer miles earned per year vs. liters of ice cream consumed per week')
     axs1_xlabel_text = axs[0][1].set_xlabel(u'flyer miles earned per year')
     axs1_ylabel_text = axs[0][1].set_ylabel(u'Liters of ice cream consumed per week')
@@ -154,150 +153,146 @@ def showdatas(datingDataMat, datingLabels):
     plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
     plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
 
-    # 画出散点图,以datingDataMat矩阵的第二(玩游戏)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
+    # Plot scatter diagram, use the data in second(playing the game) and the third colum(eating iceCream) of datingDataMat matrix to plot the figure. The size of the scatter points is 15, transparency is 0.5  
     axs[1][0].scatter(x=datingDataMat[:, 1], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
-    # 设置标题,x轴label,y轴label
+    # Set the titlef for both x-axis and y-axis 
     axs2_title_text = axs[1][0].set_title(u'Percentage of time consumed playing video games vs. liters of ice cream consumed per week')
     axs2_xlabel_text = axs[1][0].set_xlabel(u'Percentage of time consumed by playing video games')
     axs2_ylabel_text = axs[1][0].set_ylabel(u'Liters of ice cream consumed per week')
     plt.setp(axs2_title_text, size=9, weight='bold', color='red')
     plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
     plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
-    # 设置图例
+    # Setting legend
     didntLike = mlines.Line2D([], [], color='black', marker='.',
                               markersize=6, label='didntLike')
     smallDoses = mlines.Line2D([], [], color='orange', marker='.',
                                markersize=6, label='smallDoses')
     largeDoses = mlines.Line2D([], [], color='red', marker='.',
                                markersize=6, label='largeDoses')
-    # 添加图例
+    # Adding legend
     axs[0][0].legend(handles=[didntLike, smallDoses, largeDoses])
     axs[0][1].legend(handles=[didntLike, smallDoses, largeDoses])
     axs[1][0].legend(handles=[didntLike, smallDoses, largeDoses])
-    # 显示图片
+    # Showing pictures
     plt.show()
 
 
 """
-函数说明:对数据进行归一化
-
+Function Description: Normalize the Data
 Parameters:
-	dataSet - 特征矩阵
+	dataSet - Characteristic matrix
 Returns:
-	normDataSet - 归一化后的特征矩阵
-	ranges - 数据范围
-	minVals - 数据最小值
+	normDataSet - Normlized Characteristic matrix
+	ranges - Range of the data
+	minVals - The minimum value of the data
 
 Modify:
-	2017-03-24
+	2020-10-30
 """
 
 
 def autoNorm(dataSet):
-    # 获得数据的最小值
+    # Got the minimum value of the data
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
-    # 最大值和最小值的范围
+    # The range of the minimum and maximum values
     ranges = maxVals - minVals
-    # shape(dataSet)返回dataSet的矩阵行列数
+    # shape(dataSet)return the number of the columns amd rows of dataSet matrix
     normDataSet = np.zeros(np.shape(dataSet))
-    # 返回dataSet的行数
+    # return the number of rows of dataSet
     m = dataSet.shape[0]
-    # 原始值减去最小值
+    # Primary value minus the minimum value
     normDataSet = dataSet - np.tile(minVals, (m, 1))
-    # 除以最大和最小值的差,得到归一化数据
+    # The normalized data is obtained by dividing the difference between the maximum and minimum values
     normDataSet = normDataSet / np.tile(ranges, (m, 1))
-    # 返回归一化数据结果,数据范围,最小值
+    # Returns the normalized data result, data range, and minimum value
     return normDataSet, ranges, minVals
 
 
 """
-函数说明:分类器测试函数
-取百分之十的数据作为测试数据，检测分类器的正确性
-
+Function Description: Classifier Testing Function
+Take ten percent of the data as test data to test the correctness of the classifier
 Parameters:
-	无
+	None
 Returns:
-	无
+	None
 
 Modify:
-	2017-03-24
+	2020-10-30
 """
 
 
 def datingClassTest():
-    # 打开的文件名
+    # Opne the name of the file
     filename = "datingTestSet.txt"
-    # 将返回的特征矩阵和分类向量分别存储到datingDataMat和datingLabels中
+    # Store the returned feature matrix and classification vector into the datingDataMat and datingLabels respectively
     datingDataMat, datingLabels = file2matrix(filename)
     showdatas(datingDataMat, datingLabels)
-    # 取所有数据的百分之十
+    # Take ten percent of all data
     hoRatio = 0.10
-    # 数据归一化,返回归一化后的矩阵,数据范围,数据最小值
+    # Data normalization, return the normalized matrix, data range, data minimum
     normMat, ranges, minVals = autoNorm(datingDataMat)
-    # 获得normMat的行数
+    # Got the number of the columns of normMat
     m = normMat.shape[0]
-    # 百分之十的测试数据的个数
+    # The number of ten percent of the testing data 
     numTestVecs = int(m * hoRatio)
-    # 分类错误计数
+    # Classification error counting
     errorCount = 0.0
 
     for i in range(numTestVecs):
-        # 前numTestVecs个数据作为测试集,后m-numTestVecs个数据作为训练集
+        # The numTestVecs are used as the test set, and the m-numTestVecs are used as the training set.
         classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
                                      datingLabels[numTestVecs:m], 4)
-        print("分类结果:%s\t真实类别:%d" % (classifierResult, datingLabels[i]))
+        print("Classifying Result:%s\tReal Category:%d" % (classifierResult, datingLabels[i]))
         if classifierResult != datingLabels[i]:
             errorCount += 1.0
-    print("错误率:%f%%" % (errorCount / float(numTestVecs) * 100))
+    print("Rate of Error:%f%%" % (errorCount / float(numTestVecs) * 100))
 
 
 """
-函数说明:通过输入一个人的三维特征,进行分类输出
-
+Function Description: By inputting the 3D features of a person, the output is classified
 Parameters:
-	无
+	None
 Returns:
-	无
+	None
 
 Modify:
-	2017-03-24
+	2020-10-30
 """
 
 
 def classifyPerson():
-    # 输出结果
-    resultList = ['讨厌', '有些喜欢', '非常喜欢']
-    # 三维特征用户输入
-    precentTats = float(input("玩视频游戏所耗时间百分比:"))
-    ffMiles = float(input("每年获得的飞行常客里程数:"))
-    iceCream = float(input("每周消费的冰激淋公升数:"))
-    # 打开的文件名
+    # Output result
+    resultList = ['Dislike', 'Just a little like', 'Really love']
+    # 3-D features of the user imput
+    precentTats = float(input("Percentage of time spent playing video games:"))
+    ffMiles = float(input("Number of frequent flyer miles earned per year:"))
+    iceCream = float(input("Liters of ice cream consumed per week:"))
+    # The name of the file opened
     filename = "datingTestSet.txt"
-    # 打开并处理数据
+    # Open and process data
     datingDataMat, datingLabels = file2matrix(filename)
-    # 训练集归一化
+    # Normalize  the training set
     normMat, ranges, minVals = autoNorm(datingDataMat)
-    # 生成NumPy数组,测试集
+    # Generate the array of Numpy and testing set
     inArr = np.array([ffMiles, precentTats, iceCream])
-    # 测试集归一化
+    # Normalize the testing set
     norminArr = (inArr - minVals) / ranges
-    # 返回分类结果
+    # Return the classifying results
     classifierResult = classify0(norminArr, normMat, datingLabels, 3)
-    # 打印结果
-    print("你可能%s这个人" % (resultList[classifierResult - 1]))
+    # Print the result
+    print("Maybe you %s this guy" % (resultList[classifierResult - 1]))
 
 
 """
-函数说明:main函数
-
+Function Description: main function
 Parameters:
 	None
 Returns:
-	无
+	None
 
 Modify:
-	2017-03-24
+	2021-10-30
 """
 if __name__ == '__main__':
     datingClassTest()
